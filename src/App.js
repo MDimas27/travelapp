feef31f19aa478b82cb5dafad31c31dc11d0cabd
MDimas27 <mdimas.umb@gmail.com>
2022-07-04 16:57:03 +0700

    Added my project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "assets/scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      Hai dunia tipu2!
+    </div>
+  );
+}
+
+export default App;
